cbf2536f2c70541dd31570e61fe10dff21cf925f
Mustafa DAĞTEKİN <mustafa.dagtekin@gmail.com>
2019-07-11 02:07:17 +0300

    IntroCsharp

--- IntroCsharp/Form1.Designer.cs
+++ IntroCsharp/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(24, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tıklayınız";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(218, 248);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- IntroCsharp/Form1.cs
+++ IntroCsharp/Form1.cs
@@ -16,5 +16,17 @@ namespace IntroCsharp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //butona basıldığında 
+            //Merhaba mesajını göster
+            /*
+             * Bu da 
+             * Açıklama
+             * Satırı
+             */
+            MessageBox.Show("Merhaba");
+        }
     }
 }
